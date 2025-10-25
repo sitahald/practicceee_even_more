765b5701bd0afd4e9f1eebaded5f967c07a104d4
sitahald <sitahald@sheridancollege.ca>
2025-10-25 09:42:39 -0400

    1st change

---
+++ src/main/java/git/practiceeeeeeeeee/Practiceeeeeeeeee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package git.practiceeeeeeeeee;
+
+/**
+ *
+ * @author wwjde
+ */
+public class Practiceeeeeeeeee {
+
+    public static void main(String[] args) {
+        System.out.println("practice 1");
+    }
+}
